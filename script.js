e7f645263fa7ba5af959241829fb16d66c6a71c0
J-On-Prod <djjd50@hotmail.fr>
2025-06-30 22:45:12 +0200

    add for 3D background

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const logos = document.querySelectorAll('.logo');
 
+const descriptions = {
+	"Next.js": "Next.js is a Node.js framework with : React, Router, Middleware, ... <a href='https://nextjs.org/'>Next.js Website</a>",
+	"Python": "",
+	"Java": "",
+	"Bash": "",
+	"Power Platform": "",
+	"Powershell": "",
+	"Azure": "",
+	"Sharepoint": "",
+	"Arduino": "",
+};
+
+function changeDescriptions(technoName) {
+	console.log(technoName);
+	if (technoName in descriptions) {
+		const descriptionDiv = document.querySelector('#technology-description');
+		console.log(technoName, descriptionDiv);
+		descriptionDiv.innerHTML = descriptions[technoName];
+	}
+}
+
 function logoOnOver(eventLogo) {
 	let altName = "";
 	const localName = eventLogo.target.localName;
@@ -9,6 +30,7 @@ function logoOnOver(eventLogo) {
 	} else if (localName == "div" && className == "logo") {
 		altName = eventLogo.target.firstElementChild.alt;
 	}
+	changeDescriptions(altName);
 }
 
 logos.forEach((logo) => {
