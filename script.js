681744ab72d3185fd7def9e2257a08cb4e6bb296
J-On-Prod <djjd50@hotmail.fr>
2025-10-01 16:50:04 +0200

    Set footer padding

--- script.js
+++ script.js
@@ -27,15 +27,24 @@ const descriptionProjects = {
 };
 
 const descriptionsTechnos = {
-	"Next.js": "<p>Next.js is a Node.js (based on JavaScript) framework with : React, Router, Middleware, ... </p><p><a href='https://nextjs.org/'>Next.js website</a></p>",
-	"Python": "<p>Python is a interpreted language with flexible syntax for develop extermely speedly scritps, projects, ... </p><p><a href='https://www.python.org/'>Python website</a></p>",
-	"Java": "<p>OOP Language can be transportable on other OS (Windows, Linux, MacOS, Android). It's used on a lot of popular projects </p><p><a href='https://www.java.com'>Java website</a></p>",
-	"Bash": "<p>Terminal language for linux, really popular on developer community. </p><p><a href='https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html'>BASH manual</a></p>",
-	"Power Platform": "<p>Low Code develop by Microsoft, Could create interface and flow with less code and more.</p><p><a href='https://www.microsoft.com/power-platform'>Power Platform website</a></p>",
-	"PowerShell": "<p>Terminal language for Microsoft and replace the legacy command line.  </p><p><a href='https://learn.microsoft.com/powershell/scripting/overview?view=powershell-7.5'>PowerShell presentation</a></p>",
-	"Azure": "<p>Platform who groups a lot of Microsoft services : Administration Panels, Function Apps, Synapse, ... </p><p><a href='https://azure.microsoft.com'>Azure website</a></p>",
-	"SharePoint": "<p>Use for entreprise for sharing documents, forms, news, ... </p><p><a href='https://www.microsoft.com/microsoft-365/sharepoint/collaboration'>SharePoint website</a></p>",
-	"Arduino": "<p>Framework in C use for embedded system like Arduino Uno, ESP32, Raspberry Pico, ... </p><p><a href='https://www.arduino.cc/'>Arduino website</a></p>",
+	"Next.js": `<p><span class='code-line'>1 </span>Next.js is a Node.js (based on JavaScript) framework with : React, Router, Middleware, ... </p>
+	<p><span class='code-line'>2 </span><a href='https://nextjs.org/'>Website : Next.js</a></p>`,
+	"Python": `<p><span class='code-line'>1 </span>Python is a interpreted language with flexible syntax for develop extermely speedly scritps, projects, ... </p>
+	<p><span class='code-line'>2 </span><a href='https://www.python.org/'>Website : Python </a></p>`,
+	"Java": `<p><span class='code-line'>1 </span>OOP Language can be transportable on other OS (Windows, Linux, MacOS, Android). It's used on a lot of popular projects </p>
+	<p><span class='code-line'>2 </span><a href='https://www.java.com'>Website : Java</a></p>`,
+	"Bash": `<p><span class='code-line'>1 </span>Terminal language for linux, really popular on developer community. </p>
+	<p><span class='code-line'>2 </span><a href='https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html'>Website : BASH manual</a></p>`,
+	"Power Platform": `<p><span class='code-line'>1 </span>Low Code develop by Microsoft, Could create interface and flow with less code and more.</p>
+	<p><span class='code-line'>2 </span><a href='https://www.microsoft.com/power-platform'>Website : Power Platform</a></p>`,
+	"PowerShell": `<p><span class='code-line'>1 </span>Terminal language for Microsoft and replace the legacy command line.</p>
+	<p><span class='code-line'>2 </span><a href='https://learn.microsoft.com/powershell/scripting/overview?view=powershell-7.5'>Website : PowerShell presentation</a></p>`,
+	"Azure": `<p><span class='code-line'>1 </span>Platform who groups a lot of Microsoft services : Administration Panels, Function Apps, Synapse, ... </p>
+	<p><span class='code-line'>2 </span><a href='https://azure.microsoft.com'>Website : Azure</a></p>`,
+	"SharePoint": `<p><span class='code-line'>1 </span>Use for entreprise for sharing documents, forms, news, ... </p>
+	<p><span class='code-line'>2 </span><a href='https://www.microsoft.com/microsoft-365/sharepoint/collaboration'>Website : SharePoint</a></p>`,
+	"Arduino": `<p><span class='code-line'>1 </span>Framework in C use for embedded system like Arduino Uno, ESP32, Raspberry Pico, ... </p>
+	<p><span class='code-line'>2 </span><a href='https://www.arduino.cc/'>Website : Arduino</a></p>`,
 };
 
 /**
